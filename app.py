5938b80547d9a0365de564fad7f0fa8f9239def2
mheronanh <60220000+mheronanh@users.noreply.github.com>
2023-08-07 00:07:06 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,41 +4,33 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import time
 import plotly.graph_objects as go
+from supabase import create_client, Client
+import json
 
-data = "data kebisingan(1)-rows.csv"
+@st.cache_resource
+def init_connection():
+    url = st.secrets["supabase_url"]
+    key = st.secrets["supabase_key"]
+    return create_client(url, key)
 
-@st.experimental_memo
-def get_data() -> pd.DataFrame:
-    return pd.read_csv(data, delimiter=";")
+@st.cache_data(ttl=15)
+def run_query():
+    return supabase.table("mytable").select("*").order("kebisingan", desc=True).limit(4).execute()
+
+def convert_dict_to_df(data):
+    df = pd.DataFrame.from_dict(data)
+    return df
 
 st.title("Real-Time / Live Data Science Dashboard")
 
-df = get_data()
 placeholder = st.empty()
-i = 4
-while i < 636:
-    with placeholder.container():
-        st.dataframe(df[i-4:i]) 
-        z = df.iloc[(i-4):i, 1:5].values
-        st.write(z)
-        fig = go.Figure(data=
-            go.Contour(
-                z = z,
-                contours = dict(
-                    coloring ='heatmap',
-                    showlabels = True,
-                    labelfont = dict(
-                        size = 10,
-                        color = 'white',
-                    )
-                )
-            ))
-        for j in range(4):
-            for k in range(4):
-                fig.add_annotation(x=j, y=k, text=str(z[j,k]), showarrow=False, font_size=16, font_color='black', bgcolor='white', opacity=0.75 )
-        fig.update_layout(margin=dict(l=10, r=10, b=10, pad=10), plot_bgcolor='white')
-        st.plotly_chart(fig, theme="streamlit")
-        i += 4
-        time.sleep(1)
-print("Done")
-
+while True:
+    supabase = init_connection()
+    rows = run_query()
+    rows = rows.model_dump_json()
+    rows = json.loads(rows)
+    if len(rows['data']) == 4:
+        with placeholder.container():
+            df = convert_dict_to_df(rows['data'])
+            st.write(df)
+    time.sleep(5)
